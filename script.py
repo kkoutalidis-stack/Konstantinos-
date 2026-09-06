3faa6e48d04e437d7486c9f8ab381e0aa6887ec4
kkoutalidis-stack <kkoutalidis@gmail.com>
2026-09-06 22:49:04 +0300

    Increase wind threshold from 5 to 25 knots

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ import requests
 from playwright.sync_api import sync_playwright
 
 URL = "https://kiting.live/kitesurf-spot/paroskite-paros-greece"
-THRESHOLD_KNOTS = 5
+THRESHOLD_KNOTS = 25
 NTFY_TOPIC = os.environ.get("NTFY_TOPIC", "")
 STATE_FILE = "state.json"
 
